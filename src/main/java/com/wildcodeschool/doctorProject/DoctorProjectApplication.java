3e99fa13c154721485f8a63a216941f9699bd6c5
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2022-08-30 15:23:00 +0200

    first commit

---
+++ src/main/java/com/wildcodeschool/doctorProject/DoctorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorProjectApplication.class, args);
+	}
+
+}
